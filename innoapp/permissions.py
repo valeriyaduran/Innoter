a2e64562321cc8da9d0ad73e46f52514fedae00a
valeriyaduran <valeriya.duran@gmail.com>
2022-10-01 21:23:09 +0300

    several permissions added for user, page

--- innoapp/permissions.py
+++ innoapp/permissions.py
@@ -1,4 +1,22 @@
+import datetime
+
+
 from rest_framework import permissions
+from rest_framework.exceptions import NotFound
+
+
+class IsStaffOrDontSeeBlockedPage(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not request.user.is_staff:
+            if obj.unblock_date > datetime.datetime.utcnow():
+                raise NotFound("The page is not found")
+
+
+class IsStaffOrDontSeeBlockedUser(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if obj.is_blocked:
+            if not request.user.is_staff:
+                raise NotFound("The page is not found")
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -6,3 +24,4 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if obj.owner == request.user or request.user.is_staff:
             return True
         return False
+
